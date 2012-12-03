cf50a56d9d3832aff0997d81eb040eb0d60edfb7
Vlad <vg@vladgh.com>
2012-12-03 09:33:28 -0800

    Update PupetLabs APT Repo Signed-off-by: Vlad <vg@vladgh.com>

--- manifests/repo_puppetlabs.pp
+++ manifests/repo_puppetlabs.pp
@@ -21,12 +21,24 @@
 #
 class common::repo_puppetlabs {
 
-  apt::source { 'puppetlabs.com':
-    location    => 'http://apt.puppetlabs.com',
-    key         => '4BD6EC30',
-    key_server  => 'pgp.mit.edu',
-    include_src => true,
+  common::wget {'PuppetLabs Release':
+    source      => "http://apt.puppetlabs.com/puppetlabs-release-${::lsbdistcodename}.deb",
+    destination => $common::debs_dir,
   }
+
+  package{'PuppetLabs Release':
+    ensure   => present,
+    provider => dpkg,
+    source   => "${common::debs_dir}/puppetlabs-release-${::lsbdistcodename}.deb",
+    require  => Common::Wget ['PuppetLabs Release'],
+  }
+
+  #apt::source { 'puppetlabs':
+    #location    => 'http://apt.puppetlabs.com',
+    #key         => '4BD6EC30',
+    #key_server  => 'pgp.mit.edu',
+    #include_src => true,
+  #}
 
 }
 
